ea7d9da80ce62b6e0026fd7457278e27fe48b65e
Anh <97702902+hihaianh@users.noreply.github.com>
2023-12-22 14:11:20 +0000

    files added
    js, css, html files added

---
+++ books.js
@@ -0,0 +1,146 @@
+const books = [
+    {
+        id: 1,
+        name: 'The Gruffalo',
+        author: '',
+        genre: `Children's Fiction`,
+        who: 'Early Readers',
+        image: ''
+    },
+    {
+        id: 2,
+        name: 'Mog the Forgetful Cat',
+        author: '',
+        genre: `Children's Fiction`,
+        who: 'Early Readers',
+        image: ''
+    },
+    {
+        id: 3,
+        name: 'The Highway Rat',
+        author: 'Julia Donaldson and Axel Scheffler',
+        genre: `Children's Fiction`,
+        who: 'Early Readers',
+        image: ''
+    },
+    {
+        id: 4,
+        name: 'Claude in the Spotlight',
+        author: 'Alex T. Smith',
+        genre: `Children's Fiction`,
+        who: 'Early Readers',
+        image: 'https://cdn.waterstones.com/bookjackets/large/9781/4449/9781444909296.jpg'
+    },
+    {
+        id: 5,
+        name: `Handa's Surprise`,
+        author: 'Eileen Browne',
+        genre: `Children's Fiction`,
+        who: 'Early Readers',
+        image: 'https://m.media-amazon.com/images/I/A1emTH3XnfL._SY425_.jpg'
+    },
+    {
+        id: 6,
+        name: '',
+        author: '',
+        genre: `Children's Fiction`,
+        who: 'Early Readers',
+        image: ''
+    },
+    {
+        id: 7,
+        name: '',
+        author: '',
+        genre: ``,
+        who: 'young adults',
+        image: ''
+    },
+    {
+        id: 8,
+        name: '',
+        author: '',
+        genre: ``,
+        who: 'young adults',
+        image: ''
+    },
+    {
+        id: 9,
+        name: '',
+        author: '',
+        genre: ``,
+        who: 'young adults',
+        image: ''
+    },
+    {
+        id: 10,
+        name: '',
+        author: '',
+        genre: '',
+        who: 'young adults',
+        image: ''
+    },
+    {
+        id: 11,
+        name: '',
+        author: '',
+        genre: '',
+        who: 'young adults',
+        image: ''
+    },
+    {
+        id: 12,
+        name: '',
+        author: '',
+        genre: '',
+        who: 'young adults',
+        image: ''
+    },
+    {
+        id: 13,
+        name: '',
+        author: '',
+        genre: '',
+        who: 'young adults',
+        image: ''
+    },
+    {
+        id: 14,
+        name: '',
+        author: '',
+        genre: '',
+        who: 'young adults',
+        image: ''
+    },
+    {
+        id: 15,
+        name: '',
+        author: '',
+        genre: '',
+        who: 'young adults',
+        image: ''
+    },
+    {
+        id: 16,
+        name: '',
+        author: '',
+        genre: '',
+        who: 'young adults',
+        image: ''
+    },
+    {
+        id: 17,
+        name: '',
+        author: '',
+        genre: '',
+        who: 'young adults',
+        image: ''
+    },
+    {
+        id: 18,
+        name: '',
+        author: '',
+        genre: '',
+        who: 'young adults',
+        image: ''
+    }
+]
